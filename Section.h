9769156c520d221fdd3c41196f233aea46001498
mcnettp <mcnettp@gmail.com>
2016-03-31 16:06:45 -0400

    Update Section.h
    Updated to work with seat rows instead of individual seats

--- Section.h
+++ Section.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "Seat_Row.h"
 #include  "Seat.h"
+
 class Section
 {
 public: 
 	//WHAT IS THE MAXIMUM?
-	const static int MAX_SEATS = 1000;
+	const static int MAX_SEAT_ROWS = 1000;
 private:
 	string section_name;
-	const Seat* seats[MAX_SEATS];
-	int number_of_seats;
+	const Seat_Row* seat_rows[MAX_SEAT_Rows];
+	int number_of_seat_rows;
 
 public:
 	Section(string section_name_);
 	~Section();
 
-	void Section::Add_Seat(const Seat* new_seat);
+	void Section::Add_Seat_Row(const Seat_Row* new_seat_row);
 };
 
